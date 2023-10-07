bc9997fafcb09999e9dabbe0eb20e0448fbd6872
SPro <prokopenko89@gmail.com>
2023-10-07 13:32:28 +0300

    added curriculum entity

---
+++ src/main/java/ua/foxminded/task31/entity/Curriculum.java
@@ -0,0 +1,34 @@
+package ua.foxminded.task31.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.util.Map;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "curriculum")
+public class Curriculum {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "curriculum_id")
+    private Long id;
+
+    @ManyToOne(
+            cascade = {
+                    CascadeType.PERSIST,
+                    CascadeType.MERGE
+            }
+    )
+    @JoinColumn(name = "group_id", referencedColumnName = "group_id")
+    private Group group;
+
+    @ElementCollection
+    @CollectionTable(name = "number_of_lessons", joinColumns = @JoinColumn(name = "curriculum_id"))
+    @MapKeyColumn(name = "course_id")
+    @Column(name = "number_of_lessons")
+    private Map<Integer, Integer> numberOfLessons;
+}
--- src/main/java/ua/foxminded/task31/model/Сurriculum.java
+++
@@ -1,9 +0,0 @@
-package ua.foxminded.task31.model;
-
-public class Сurriculum {
-    private int[][] curriculum;
-
-    public int[][] getCurriculum() {
-        return curriculum;
-    }
-}
